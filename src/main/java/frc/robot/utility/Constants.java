2e23a961acc9782e59c8e97452100cf0c77b6769
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2024-02-01 17:57:03 -0500

    new ids

--- src/main/java/frc/robot/utility/Constants.java
+++ src/main/java/frc/robot/utility/Constants.java
@@ -18,27 +18,27 @@ public final class Constants {
     public static final double DRIVETRAIN_TRACKWIDTH_METERS = Units.inchesToMeters(19.5);
     public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(21.5);
 
-    public static final int FRONT_LEFT_DRIVE_MOTOR = 9;
-    public static final int FRONT_LEFT_TURN_MOTOR = 8;
-    public static final int FRONT_LEFT_ENCODER = 14;
+    public static final int FRONT_LEFT_DRIVE_MOTOR = 4;
+    public static final int FRONT_LEFT_TURN_MOTOR = 5;
+    public static final int FRONT_LEFT_ENCODER = 11;
     public static double FRONT_LEFT_ENCODER_OFFSET;
 
-    public static final int FRONT_RIGHT_DRIVE_MOTOR = 5;
-    public static final int FRONT_RIGHT_TURN_MOTOR = 6;
-    public static final int FRONT_RIGHT_ENCODER = 15;
+    public static final int FRONT_RIGHT_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_TURN_MOTOR = 3;
+    public static final int FRONT_RIGHT_ENCODER = 10;
     public static double FRONT_RIGHT_ENCODER_OFFSET;
 
-    public static final int BACK_LEFT_DRIVE_MOTOR = 10;
-    public static final int BACK_LEFT_TURN_MOTOR = 11;
-    public static final int BACK_LEFT_ENCODER = 13;
+    public static final int BACK_LEFT_DRIVE_MOTOR = 6;
+    public static final int BACK_LEFT_TURN_MOTOR = 7;
+    public static final int BACK_LEFT_ENCODER = 12;
     public static double BACK_LEFT_ENCODER_OFFSET;
 
-    public static final int BACK_RIGHT_DRIVE_MOTOR = 4;
-    public static final int BACK_RIGHT_TURN_MOTOR = 3;
-    public static final int BACK_RIGHT_ENCODER = 16;
+    public static final int BACK_RIGHT_DRIVE_MOTOR = 8;
+    public static final int BACK_RIGHT_TURN_MOTOR = 9;
+    public static final int BACK_RIGHT_ENCODER = 13;
     public static double BACK_RIGHT_ENCODER_OFFSET;
 
-    public static final int PIGEON_ID = 19;
+    public static final int PIGEON_ID = 14;
 
     public static final double WHEEL_DIAMETER = 0.10033; // Metres
     public static final double DRIVE_REDUCTION = (15.0 / 32.0) * (10.0 / 60.0);
