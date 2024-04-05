58999a05f0d0c5d2dcc5fa5e1b413f243cdef61c
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2024-04-05 08:22:18 -0400

    small changes

--- src/main/java/frc/robot/modules/KrakenSwerveModule.java
+++ src/main/java/frc/robot/modules/KrakenSwerveModule.java
@@ -45,8 +45,8 @@ public class KrakenSwerveModule {
         TalonFXConfiguration config = new TalonFXConfiguration();
 
         steerMotor.setSmartCurrentLimit(20);
-            config.CurrentLimits.StatorCurrentLimit = 80;
-            config.CurrentLimits.StatorCurrentLimitEnable = true;
+        config.CurrentLimits.StatorCurrentLimit = 80;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
         config.CurrentLimits.SupplyCurrentLimit = 20;
         config.CurrentLimits.SupplyCurrentLimitEnable = true;
 
